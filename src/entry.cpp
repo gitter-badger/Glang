355a310b48d533516070bdaab9fba55d9c948e14
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-12-09 14:41:05 -0600

    Declare new instance of lexer and call 'tokenize'

--- src/entry.cpp
+++ src/entry.cpp
@@ -7,9 +7,12 @@
 
 #import <iostream>
 #import "misc.hpp"
+#import "Lexer/Lexer.hpp"
 
 int main(int argc, const char **argv)
 {
+    Lexer myLexer = Lexer();
+
     // NOTE: We start 'argInd' at 1 because we don't want
     //  to do anything with the first argument (the program).
     for (int argInd = 1; argInd < argc; argInd++)
@@ -17,6 +20,8 @@ int main(int argc, const char **argv)
         printf("Current input file: %s\n", *(argv + argInd));
 
         std::string currentFileVal = getFile(*(argv + argInd));
+
+        myLexer.tokenize(currentFileVal);
 
         // TODO (Gigabyte Giant): Tokenize the current file, then parse.
     }
