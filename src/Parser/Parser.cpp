825b5051fcd3f1f0f9cf7d6a19ce309478c0e938
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-12-09 12:45:46 -0600

    Create interface and implementation for 'Parser'

---
+++ src/Parser/Parser.cpp
@@ -0,0 +1,13 @@
+//
+// Parser.cpp
+//
+// Parser class implementation
+// Created by Gigabyte Giant on 12/09/2015
+//
+
+#import "Parser.hpp"
+
+Parser::Parser()
+{
+    
+}
---
+++ src/Parser/Parser.hpp
@@ -0,0 +1,17 @@
+//
+// Parser.hpp
+//
+// Parser class interface
+// Created by Gigabyte Giant on 12/09/2015
+//
+
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+class Parser
+{
+    public:
+        Parser();
+};
+
+#endif
