530c8b03d60f790abb4dd5f580fcb47f050f17b9
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-12-09 13:47:02 -0600

    Create header file that contains macros used to...
    ...determine what 'type' a character is.

---
+++ src/Lexer/tokenRules.h
@@ -0,0 +1,13 @@
+//
+// tokenRules.h
+//
+// Defines all of the macros used when determining what a character is.
+// Created by Gigabyte Giant on 12/09/2015
+//
+
+#ifndef __TOKENRULES_H__
+#define __TOKENRULES_H__
+
+#define isIdentifier(c) ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+
+#endif
