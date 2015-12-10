6287e9832e97edc9ff48a36596b9f07a3986f5d0
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-12-10 12:09:49 -0600

    Add TODO comment(s) to Lexer class

--- src/Lexer/Lexer.hpp
+++ src/Lexer/Lexer.hpp
@@ -4,6 +4,8 @@
 // Lexer class interface
 // Created by Gigabyte Giant on 12/09/2015
 //
+// TODO (Gigabyte Giant): Completely rewrite Lexer class interface.
+//
 
 #ifndef __LEXER_H__
 #define __LEXER_H__
